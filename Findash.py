6614e15e6eac27760484994e3ab428b65ce4790a
Sumit Joshi <112744587+shubhu31999@users.noreply.github.com>
2022-10-13 12:40:13 +0530

    Add files via upload

---
+++ Findash.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  8 21:07:10 2022
+
+@author: shubhu
+"""
+
+import pandas as pd
+import streamlit as st
+
+data=pd.read_csv("C:\\Users\\shubhu\\Desktop\\financial_dash.csv")
+df=pd.DataFrame(data)
+df=df.set_index('symbol')
+
+dropdown1=st.selectbox('Choose the sector',df.sector.unique())
+dropdown2=st.selectbox('Choose the parametre',df.columns[df.columns!='sector'])
+
+values=df[df.sector==dropdown1][[dropdown2]]
+
+st.bar_chart(values)
